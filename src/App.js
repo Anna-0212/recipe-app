340c859e62ad885a6b4d148bfd338c61fa194526
Anna-0212 <112332240+Anna-0212@users.noreply.github.com>
2022-11-25 16:07:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,22 +8,26 @@ function App() {
     const MY_ID = "e037ef8f";
     const MY_KEY = "55499c31704b6fb0942653b077e73afb";
 
+ 
   const [mySearch, setMySearch] = useState('');
   const [myRecipes, setMyRecipes] = useState([]);
-  const [wordSubmitted, setWordSubmitted] = useState("avocado")
+  const [wordSubmitted, setWordSubmitted] = useState("avocado");
 
-    useEffect(async () => {
+    useEffect(() => {
+      const getRecipes = async () => {
       const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${MY_ID}&app_key=${MY_KEY}`);
       const data = await response.json();
       setMyRecipes(data.hits);
-      console.log(data.hits);
+     
+      }
+      getRecipes()
+      
     }, [wordSubmitted])
 
   const myRecipeSearch = (e) => {
-    setMySearch(e.target.value)
-    console.log(e.target.value)
+    setMySearch(e.target.value);
   }
-
+  
   const finalSearch = (e) => {
     e.preventDefault();
     setWordSubmitted(mySearch);
@@ -37,17 +41,20 @@ function App() {
         </video>
         <h1>Find a Recipe</h1>
       </div>
+
       <div className='container'>
         <form onSubmit={finalSearch}>
           <input className="search" placeholder='Search...' onChange={myRecipeSearch} value={mySearch} >
           </input>
         </form>
       </div>
+      
       <div className='container'> 
         <button>
           <img src="https://img.icons8.com/fluency/48/000000/fry.png" className="icons" alt="search"/>
         </button>
       </div>
+      
       <div className='container'> 
         {myRecipes.map(element => (
           <MyRecipesComponent 
